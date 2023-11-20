191ab62847263091369c60c5f62008219d164a18
Jake Correia <jacobrcorreia@gmail.com>
2023-11-19 19:16:46 -0800

    Final Draft

--- js/script-ems.js
+++ js/script-ems.js
@@ -1,9 +1,11 @@
+// localStorage.clear(); // clears local storage for testing purposes
+
 // CREATE AN ARRAY OF EMPLOYEES
 let employees = [
     [12345678, 'John Smith', 1234, 'johnsmith@randomemail.com', 'Marketing'],
     [23456789, 'Jane Doe', 2345, 'janedoe@randomemail.com', 'Sales'],
     [34567890, 'Bob Johnson', 3456, 'bobjohnson@randomemail.com', 'Administrative'],
-    [11110000, 'Elvis Presley', 1111, 'elvispresley@randomemail.com', 'QA'],
+    [11110000, 'Elvis Presley', 1111, 'elvispresley@randomemail.com', 'Quality Assurance'],
     [11112222, 'Abraham Lincoln', 2222, 'abrahamlincoln@randomemail.com', 'Executive']
 ];
 
@@ -52,22 +54,19 @@ form.addEventListener('submit', (e) => {
 });
 
 // DELETE EMPLOYEE
-empTable.addEventListener('click', (e) => {
+function deleteEmployee(e) {
     // CONFIRM THE DELETE
-    if (e.target.classList.contains('delete')) {
+    if (confirm('Are you sure you want to delete this employee?')) {
+        // GET THE SELECTED ROWINDEX FOR THE TR (PARENTNODE.PARENTNODE)
+        let rowIndex = e.target.parentNode.parentNode.rowIndex;
 
-        if (confirm('Are you sure you want to delete this employee?')) {
-            // GET THE SELECTED ROWINDEX FOR THE TR (PARENTNODE.PARENTNODE)
-            let rowIndex = e.target.parentNode.parentNode.rowIndex;
+        // REMOVE EMPLOYEE FROM ARRAY
+        employees.splice(rowIndex - 1, 1);
 
-            // REMOVE EMPLOYEE FROM ARRAY
-            employees.splice(rowIndex - 1, 1);
-
-            // BUILD THE GRID
-            buildGrid();
-        }
+        // BUILD THE GRID
+        buildGrid();
     }
-});
+}
 
 // BUILD THE EMPLOYEES GRID
 function buildGrid() {
@@ -80,35 +79,32 @@ function buildGrid() {
     empTable.appendChild(tbody);
 
     // LOOP THROUGH THE ARRAY OF EMPLOYEES
-    // REBUILDING THE ROW STRUCTURE
-    
-            // CREATE THE DELETE BUTTON
-            let deleteBtn = document.createElement('button');
-            deleteBtn.className = 'btn btn-sm btn-danger delete';
-            deleteBtn.innerHTML = '<i class="material-icons">delete</i>';
-            deleteBtn.addEventListener('click', deleteEmployee);
-    
-            // CREATE THE BUTTON (TD)
-            let buttonTd = document.createElement('td');
-            buttonTd.appendChild(deleteBtn);
-    
-            // LOOP THROUGH THE ARRAY OF EMPLOYEES
-            for (let employee of employees) {
-                // REBUILDING THE ROW STRUCTURE
-                let row = `
-                    <tr>
-                        <td>${employee[0]}</td>
-                        <td>${employee[1]}</td>
-                        <td>${employee[2]}</td>
-                        <td><a href="mailto:${employee[3]}">${employee[3]}</a></td>
-                        <td>${employee[4]}</td>
-                        <td>${buttonTd}</td>
-                    </tr>
-                `;
-    
-                // BIND THE TBODY TO THE EMPLOYEE TABLE
-                tbody.innerHTML += row;
-            }
+    for (let employee of employees) {
+        // CREATE THE DELETE BUTTON
+        let deleteBtn = document.createElement('button');
+        deleteBtn.className = 'btn btn-sm btn-danger delete';
+        deleteBtn.innerHTML = ' X ';
+        deleteBtn.addEventListener('click', deleteEmployee);
+
+        // CREATE THE BUTTON (TD)
+        let buttonTd = document.createElement('td');
+        buttonTd.appendChild(deleteBtn);
+
+        // REBUILDING THE ROW STRUCTURE
+        let row = `
+            <tr>
+                <td>${employee[0]}</td>
+                <td>${employee[1]}</td>
+                <td>${employee[2]}</td>
+                <td><a href="mailto:${employee[3]}">${employee[3]}</a></td>
+                <td>${employee[4]}</td>
+                <td>${buttonTd.innerHTML}</td>
+            </tr>
+        `;
+
+        // BIND THE TBODY TO THE EMPLOYEE TABLE
+        tbody.innerHTML += row;
+    }
 
     // UPDATE EMPLOYEE COUNT
     let empCount = document.getElementById('empCount');
